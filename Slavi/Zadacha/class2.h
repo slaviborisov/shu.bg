a38ec27027456e45ffb01b82e93badb285aad170
Svetoslav <Svetoslav.Borisov@emerchantpay.com>
2022-03-11 18:55:29 +0200

    commit zadacha

---
+++ Slavi/Zadacha/class2.h
@@ -0,0 +1,16 @@
+#include "class1.h"
+#pragma once
+#include <string>
+using namespace std;
+
+class Class2
+{
+  private:
+    int nomer1;
+    string text1;
+
+  public:
+    Class2(); //конструктор по подразбиране
+    Class2(int, string); //конструктор с параметри
+    int operator>(Class1); //Предефиниране на оператор >
+};
